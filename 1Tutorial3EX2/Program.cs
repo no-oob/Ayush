fe89c6f68c4e6bf462acac1b327d726fae1a88b9
no-oob <154967603+no-oob@users.noreply.github.com>
2024-08-13 19:15:14 +1000

    tuto

---
+++ 1Tutorial3EX2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Tutorial3EX2
+{
+    class Geeks
+    {
+        public static void Main(string[] args)
+        {
+            List<String> mylist = new List<String>();
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            for (int j = 0; j < cars.Length; j++)
+            {
+                mylist.Add(cars[j]);
+                
+            }
+            Console.WriteLine("Cars List:");
+            foreach (String items in mylist)
+            {
+            
+                Console.WriteLine(items);
+            }
+            Console.Read();
+        }
+    }
+}
